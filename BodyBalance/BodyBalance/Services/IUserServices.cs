1ff9ce72aa0a09a865c2b8e7dfe0bf06bb147b23
priscarelique <prisca.relique@gmail.com>
2015-11-05 16:39:13 +0100

    IUserServices commenté

--- BodyBalance/BodyBalance/Services/IUserServices.cs
+++ BodyBalance/BodyBalance/Services/IUserServices.cs
@@ -9,10 +9,38 @@ namespace BodyBalance.Services
 {
     public interface IUserServices
     {
+        /// <summary>
+        /// Create a user
+        /// </summary>
+        /// <param name="um"> A user model which contains all the information about the user</param>
+        /// <returns></returns>
         Boolean CreateUser(UserModel um);
+
+        /// <summary>
+        /// Find a user with his id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         UserModel FindUserById(String id);
+
+        /// <summary>
+        /// Update a user
+        /// </summary>
+        /// <param name="um"></param>
+        /// <returns></returns>
         Boolean UpdateUser(UserModel um);
+
+        /// <summary>
+        /// Delete a user
+        /// </summary>
+        /// <param name="um"></param>
+        /// <returns></returns>
         Boolean DeleteUser(UserModel um);
+
+        /// <summary>
+        /// Retrieves all the users
+        /// </summary>
+        /// <returns></returns>
         List<UserModel> FindAllUsers();
     }
 }
