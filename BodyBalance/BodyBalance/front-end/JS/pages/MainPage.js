dd57e6e564edf85d49eb8392847754a023840c76
Lucas PISA <luc.pisa@gmail.com>
2015-11-09 15:49:36 +0100

    Ajout bouton logout haut droite

--- BodyBalance/BodyBalance/front-end/JS/pages/MainPage.js
+++ BodyBalance/BodyBalance/front-end/JS/pages/MainPage.js
@@ -1,8 +1,10 @@
 ﻿window.app.mappers['#logout'] = function () {
     window.app.sendRestRequest('/Account/logout', 'POST', null, function () {
         window.app.clearLoginParameters();
+        $('[data-visible="loggedIn"]').hide();
         location.reload();
     });
     return false;
 }
+$('[data-visible="loggedIn"]').show();
 window.app.hrefToFunction('#main');
