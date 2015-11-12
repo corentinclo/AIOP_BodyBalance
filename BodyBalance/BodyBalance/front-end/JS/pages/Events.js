5aa6a1be136e86d98de0c41cce14ec0a3647d6d1
corentinclo <corentincloarec@hotmail.fr>
2015-11-12 12:41:04 +0100

    [FrontEnd] DONE: Events avec tri et filtres. TODO: rendre ça jolie!

--- BodyBalance/BodyBalance/front-end/JS/pages/Events.js
+++ BodyBalance/BodyBalance/front-end/JS/pages/Events.js
@@ -1,76 +1,50 @@
-﻿$(function () {
-    var typeList = new Array();
-    var actList = new Array();
-    // store filter for each group
-    var filters = {};
+﻿var typeList = new Array();
+var actList = new Array();
+window.app.sendRestRequest('/events', 'GET', null, function (data) {
+    $.each(data, function (key, val) {
+        if (jQuery.inArray(val.Type, typeList) == -1) {
+            $("#selType").append("<option value='." + val.Type + "'> " + val.Type + "</a>");
+            typeList.push(val.Type);
 
-    
+        }
+        if (jQuery.inArray(val.ActivityId, actList) == -1) {
+            $("#selAct").append("<option value='." + val.ActivityId + "'> " + val.ActivityId + "</a>");
+            actList.push(val.ActivityId);
+        }
 
-    window.app.sendRestRequest('/events', 'GET', null, function (data) {
-        $.each(data, function (key, val) {
-            if (jQuery.inArray(val.Type, typeList) == -1) {
-                var $li = $(document.createElement("li")).addClass("li").appendTo(".typeMenu");
-                $li.append("<a href='#' class='a' data-filter='." + val.Type + "'> " + val.Type + "</a>");
-                typeList.push(val.Type);
-                
-            }
-            if (jQuery.inArray(val.ActivityId, actList) == -1) {
-                var $li2 = $(document.createElement("li")).addClass("li").appendTo(".actMenu");
-                $li2.append("<a href='#' class='a' data-filter='." + val.ActivityId + "'> " + val.ActivityId + "</a>");
-                actList.push(val.ActivityId);
-            }
-            
-            var $item = $(document.createElement("div")).addClass("grid-item " + val.ActivityId + " " + val.Type).appendTo(".grid");
+        var $item = $(document.createElement("div")).addClass("grid-item " + val.ActivityId + " " + val.Type).appendTo(".grid");
 
-            var $panel = $(document.createElement("div")).addClass("panel panel-default").appendTo($item);
+        var $panel = $(document.createElement("div")).addClass("panel panel-default").appendTo($item);
 
-            var $panelHeading = $(document.createElement("div")).addClass("panel-heading").html("<h4 class='text-muted'>" + val.name + "</h4>").appendTo($panel);
+        var $panelHeading = $(document.createElement("div")).addClass("panel-heading").html("<h4 class='text-muted'>" + val.name + "</h4>").appendTo($panel);
 
-            var $panelBody = $(document.createElement("div")).addClass("panel-body").appendTo($panel);
-            $panelBody.append("<div class='act text-muted'>" + val.ActivityId + "</div>");
-            $panelBody.append("<div class='type text-muted'>" + val.Type + "</div>");
-            $panelBody.append("<p>" + val.MaxNb + " places available</p>");
-            $panelBody.append("<p>Room n°" + val.RoomId + "</p>");
-            $panelBody.append("<p>With " + val.ContributorId + "</p>");
+        var $panelBody = $(document.createElement("div")).addClass("panel-body").appendTo($panel);
+        $panelBody.append("<div class='act text-muted'>" + val.ActivityId + "</div>");
+        $panelBody.append("<div class='type text-muted'>" + val.Type + "</div>");
+        $panelBody.append("<p>" + val.MaxNb + " places available</p>");
+        $panelBody.append("<p>Room n°" + val.RoomId + "</p>");
+        $panelBody.append("<p>With " + val.ContributorId + "</p>");
 
 
-            // The date is in ISO8601 format
-            var date = dateFromISO8601(val.EventDate);
-            var timestamp = Date.parse(date);
-            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            var year = date.getFullYear();
-            var month = months[date.getMonth()];
-            var day = date.getDate();
-            var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            var weekDay = days[date.getDay()];
-            var hour = "0" + (date.getHours() - 1);
-            var min = "0" + date.getMinutes();
-            var time = weekDay + ', ' + month + ' ' + day + ' ' + year + '. ' + hour.substr(hour.length - 2) + ':' + min.substr(min.length - 2);
-            $panel.append("<div class='panel-footer text-muted'><p> <b>" + time + "</b></p>" + val.Price + "€</div>");
+        // The date is in ISO8601 format
+        var date = dateFromISO8601(val.EventDate);
+        var timestamp = Date.parse(date);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var weekDay = days[date.getDay()];
+        var hour = "0" + (date.getHours() - 1);
+        var min = "0" + date.getMinutes();
+        var time = weekDay + ', ' + month + ' ' + day + ' ' + year + '. ' + hour.substr(hour.length - 2) + ':' + min.substr(min.length - 2);
+        $panel.append("<div class='panel-footer text-muted'><p> <b>" + time + "</b></p>" + val.Price + "€</div>");
 
-            // Hiden timestamp for isotope sorting
-            $item.append("<div class='timestamp'>" + timestamp + "</div>");
-        });
-        $(".a").click(function () {
-            var $this = $(this);
-            console.log("filter: "+$this.attr('data-filter'));
-            // get group key
-            var $lip = $this.parents('.dropdown-menu');
-            var filterGroup = $lip.attr('data-filter-group');
-            console.log("fiter group:" +filterGroup);
-            // set filter for group
-            filters[filterGroup] = $this.attr('data-filter');
-            // combine filters
-            var filterValue = concatValues(filters);
-            // set filter for Isotope
-            $iso.isotope({ filter: filterValue });
-            $iso.isotope('layout');
-            console.log(filters);
-        });
-       
+        // Hiden timestamp for isotope sorting
+        $item.append("<div class='timestamp'>" + timestamp + "</div>");
     });
 
-    var $iso = $(".grid").isotope({
+    var iso = new Isotope('.grid', {
         itemSelector: '.grid-item',
         layoutMode: 'masonry',
         getSortData: {
@@ -78,12 +52,27 @@
         },
         sortBy: ['timestamp']
     });
-    
-});
 
-$(window).load(function () {
-    $iso.isotope('layout');
+    // filter with selects and checkboxes
+    var $selects = $('#selects select');
+
+    $selects.change(function () {
+        // map input values to an array
+        var filters = [];
+        var filterValue = '';
+        // filters from selects
+        $selects.each(function (i, elem) {
+            if (elem.value) {
+                if (elem.value != ".*") {
+                    filters.push(elem.value);
+                    filterValue += elem.value;
+                }
+            }
+        });
+        iso.arrange({ filter: filterValue });
+    });
 });
+    
 
 // function to convert date in ISO8601 format to a date for all browsers
 function dateFromISO8601(iso8601Date) {
