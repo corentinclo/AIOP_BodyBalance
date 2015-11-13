6e5c41b80983c3ba10c7d327e6aa6b5bcf9e87cc
corentinclo <corentincloarec@hotmail.fr>
2015-11-13 13:05:02 +0100

    [FrontEnd] Events et Activities DONE

--- BodyBalance/BodyBalance/front-end/JS/pages/Activities.js
+++ BodyBalance/BodyBalance/front-end/JS/pages/Activities.js
@@ -12,7 +12,8 @@
                             label: "This activity events",
                             className: "btn-success",
                             callback: function () {
-                                bootbox.alert("Soon...");
+                                var activityId = val.Name;
+                                showActivityEvents(activityId);
                             }
                         },
                         main: {
@@ -26,3 +27,64 @@
     });
 
 });
+
+function showActivityEvents(activityId){
+    window.app.sendRestRequest('/Activities/' + activityId + '/Events', 'GET', null, function (data) {
+        if ($(".grid").length) {
+            $(".grid").empty();
+            var $grid = $(".grid");
+        } else {
+            $(document.createElement("hr")).appendTo("#main");
+            var $grid = $(document.createElement("div")).addClass("grid").appendTo("#main");
+
+        }
+        $.each(data, function (key, val) {
+            var $item = $(document.createElement("div")).addClass("grid-item " + val.ActivityId + " " + val.Type).appendTo($grid);
+
+            var $panel = $(document.createElement("div")).addClass("panel panel-default").appendTo($item);
+
+            var $panelHeading = $(document.createElement("div")).addClass("panel-heading").html("<h4>" + val.name + "</h4>").appendTo($panel);
+
+            var $panelBody = $(document.createElement("div")).addClass("panel-body").appendTo($panel);
+            $panelBody.append("<div class='act'><b>" + val.ActivityId.toUpperCase() + "</b></div>");
+            $panelBody.append("<div class='type'><b>" + val.Type.toUpperCase() + "</b></div>");
+            $panelBody.append("<p><b>" + val.MaxNb + "</b> places available</p>");
+            $panelBody.append("<p>Room <b>n°" + val.RoomId + "</b></p>");
+            $panelBody.append("<p>With <b>" + val.ContributorId + "</b></p>");
+
+
+            // The date is in ISO8601 format
+            var date = dateFromISO8601(val.EventDate);
+            var timestamp = Date.parse(date);
+            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var year = date.getFullYear();
+            var month = months[date.getMonth()];
+            var day = date.getDate();
+            var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            var weekDay = days[date.getDay()];
+            var hour = "0" + (date.getHours() - 1);
+            var min = "0" + date.getMinutes();
+            var time = weekDay + ', ' + month + ' ' + day + ' ' + year + '. ' + hour.substr(hour.length - 2) + ':' + min.substr(min.length - 2);
+            $panel.append("<div class='panel-footer'><p> <b>" + time + "</b></p>" + val.Price + "€</div>");
+
+            // Hiden timestamp for isotope sorting
+            $item.append("<div class='timestamp'>" + timestamp + "</div>");
+        });
+        var iso = new Isotope('.grid', {
+            itemSelector: '.grid-item',
+            layoutMode: 'masonry',
+            getSortData: {
+                timestamp: '.timestamp'
+            },
+            sortBy: ['timestamp']
+        });
+    });
+};
+
+// function to convert date in ISO8601 format to a date for all browsers
+function dateFromISO8601(iso8601Date) {
+    var parts = iso8601Date.match(/\d+/g);
+    var isoTime = Date.UTC(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
+    var isoDate = new Date(isoTime);
+    return isoDate;
+}
