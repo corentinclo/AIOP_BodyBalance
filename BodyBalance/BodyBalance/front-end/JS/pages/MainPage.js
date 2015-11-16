906a1fbff9e2d3e2b9edff25f6e6f30bf0092781
Lucas PISA <luc.pisa@gmail.com>
2015-11-16 16:01:01 +0100

    [FrontEnd] Ajout d'un calendrier avec les évenèments ou on est inscrits sur la page d'arrivée à l'application

--- BodyBalance/BodyBalance/front-end/JS/pages/MainPage.js
+++ BodyBalance/BodyBalance/front-end/JS/pages/MainPage.js
@@ -22,3 +22,44 @@ window.app.sendRestRequest('/Users/' + window.app.username, 'GET', null, functio
 });
 
 window.app.hrefToFunction('#main');
+
+window.app.sendRestRequest("/Users/" + window.app.username + "/Events", "GET", null, function (data) {
+    var events = Array();
+    $.each(data, function (i, element) {
+        var temp = {};
+        temp.id = element.EventId;
+        temp.title = element.name;
+        temp.start = dateFromISO8601(element.EventDate);
+        temp.allDay = true;
+        temp.editable = false;
+        events.push(temp);
+    });
+    $('#fullcalendar').html('').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month'
+        },
+        buttonText: {
+            today: "Today"
+        },
+        defaultView: 'month',
+        lang: 'en',
+        editable: false,
+        droppable: false,
+        eventOverlap: true,
+        allDaySlot: true,
+        events: events
+    })
+
+}, function () {
+    bootbox.alert("An error occurred while loading your events.");
+});
+
+// function to convert date in ISO8601 format to a date for all browsers
+function dateFromISO8601(iso8601Date) {
+    var parts = iso8601Date.match(/\d+/g);
+    var isoTime = Date.UTC(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
+    var isoDate = new Date(isoTime);
+    return isoDate;
+}
