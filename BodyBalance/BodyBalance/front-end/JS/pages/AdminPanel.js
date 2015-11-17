65dece330533fb69622f67f7d1bbd0f6644bef5c
corentinclo <corentincloarec@hotmail.fr>
2015-11-17 11:33:50 +0100

    [FrontEnd] commit avant merge

--- BodyBalance/BodyBalance/front-end/JS/pages/AdminPanel.js
+++ BodyBalance/BodyBalance/front-end/JS/pages/AdminPanel.js
@@ -7,18 +7,24 @@
             window.app.sendRestRequest('/users', 'GET', null, function (data) {
                 $.each(data, function (key, val) {
                     var $tr = $("<tr></tr>").appendTo("#userTable tbody");
-                    $tr.append("<td>" + val.UserId + "</td><td>" + val.FirstName + "</td><td>" + val.LastName + "</td><td><button class='btn btn-primary btn-sm member'>Member</button> <button class='btn btn-primary btn-sm contrib'>Contributor</button> <button class='btn btn-primary btn-sm manager'>Manager</button> <button class='btn btn-primary btn-sm admin'>Admin</button></td><td><button class='btn btn-danger delBtn'><i class='fa fa-trash-o'></i></button></td>");
+                    $tr.append("<td><button class='btn btn-info infoBtn'><i class='fa fa-info-circle'></i></button></td>"+
+                               "<td>" + val.UserId +"</td><td>" + val.FirstName + "</td><td>" + val.LastName +"</td>"+
+                               "<td><input type='checkbox' id='membCheck' class='checkbox-toggle member'/><label for='membCheck'>Member</label>    " +
+                               "<input type='checkbox' id='contCheck' class='checkbox-toggle contrib'/><label for='contCheck'>Contributor</label>    " +
+                               "<input type='checkbox' id='managCheck' class='checkbox-toggle manager'/><label for='managCheck'>Manager</label>    " +
+                               "<input type='checkbox' id='adminCheck' class='checkbox-toggle admin'/><label for='adminCheck'>Admin</label></td>" +
+                               "<td><button class='btn btn-danger delBtn'><i class='fa fa-trash-o'></i></button></td>");
                     if (val.UserRoles.IsMember)
-                        $tr.find(".member").attr("disabled", true);
+                        $tr.find(".member").prop("checked",true);
                     if (val.UserRoles.IsContributor)
-                        $tr.find(".contrib").attr("disabled", true);
+                        $tr.find(".contrib").prop("checked", true);
                     if (val.UserRoles.IsManager)
-                        $tr.find(".manager").attr("disabled", true);
+                        $tr.find(".manager").prop("checked", true);
                     if (val.UserRoles.IsAdmin)
-                        $tr.find(".admin").attr("disabled", true);
+                        $tr.find(".admin").prop("checked", true);
 
                     $("#userInfoModal").modal({ show: false });
-                    $tr.click(function () {
+                    $(".infoBtn").click(function () {
                         var $modal = $("#userInfoModal");
                         $modal.find(".modal-body").empty();
                         $("#modalLabel").empty();
@@ -50,66 +56,147 @@
                     });
 
                     $tr.find(".member").click(function () {
-                        bootbox.confirm({
-                            message: "Do you want to upgrade this user ?",
-                            callback: function (result) {
-                                if (result) {
-                                    var data = { MemberId: val.UserId };
-                                    window.app.sendRestRequest('/members', 'POST', data, function (data) {
-                                        bootbox.alert("This user is now a member");
-                                        reloadAdminPanelPage();
-                                    },function(){
-                                        bootbox.alert("An error has occured");
-                                    });
+                        if ($(this).is(":checked")) {
+                            //UPGRADE
+                            bootbox.confirm({
+                                message: "Do you want to upgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        var d = new Date();
+                                        var month = d.getMonth() + 1;
+                                        var day = d.getDate();
+                                        var date = d.getFullYear() + '-' +
+                                                   (('' + month).length < 2 ? '0' : '') + month + '-' +
+                                                   (('' + day).length < 2 ? '0' : '') + day;
+                                        var data = { "UserId": val.UserId, "PayDate": date };
+                                        window.app.sendRestRequest('/members', 'POST', data, function (data) {
+                                            bootbox.alert("This user is now a member");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        }, 'application/json', true);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        } else {
+                            //DOWNGRADE
+                            bootbox.confirm({
+                                message: "Do you want to downgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        window.app.sendRestRequest('/members/'+val.UserId, 'DELETE', null, function (data) {
+                                            bootbox.alert("This user is not a member anymore");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        });
+                                    }
+                                }
+                            });
+                        }
                                 
                     });
                     $tr.find(".contrib").click(function () {
-                        bootbox.confirm({
-                            message: "Do you want to upgrade this user ?",
-                            callback: function (result) {
-                                if (result) {
-                                    window.app.sendRestRequest('/contributors', 'POST', null, function (data) {
-                                        bootbox.alert("This user is now a contributor");
-                                        reloadAdminPanelPage();
-                                    }, function () {
-                                        bootbox.alert("An error has occured");
-                                    });
+                        if ($(this).is(":checked")) {
+                            //UPGRADE
+                            bootbox.confirm({
+                                message: "Do you want to upgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        var data = { "UserId": val.UserId };
+                                        window.app.sendRestRequest('/contributors', 'POST', data, function (data) {
+                                            bootbox.alert("This user is now a contributor");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        }, 'application/json', true);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        } else {
+                            //DOWGRADE
+                            bootbox.confirm({
+                                message: "Do you want to downgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        window.app.sendRestRequest('/contributors/' + val.UserId, 'DELETE', null, function (data) {
+                                            bootbox.alert("This user is not a contributor anymore");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        });
+                                    }
+                                }
+                            });
+                        }
                     });
+
                     $tr.find(".manager").click(function () {
-                        bootbox.confirm({
-                            message: "Do you want to upgrade this user ?",
-                            callback: function (result) {
-                                if (result) {
-                                    window.app.sendRestRequest('/managers', 'POST', null, function (data) {
-                                        bootbox.alert("This user is now a manager");
-                                        reloadAdminPanelPage();
-                                    }, function () {
-                                        bootbox.alert("An error has occured");
-                                    });
+                        if ($(this).is(":checked")) {
+                            //UPGRADE
+                            bootbox.confirm({
+                                message: "Do you want to upgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        var data = { "UserId": val.UserId };
+                                        window.app.sendRestRequest('/managers', 'POST', data, function (data) {
+                                            bootbox.alert("This user is now a manager");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        }, 'application/json', true);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        } else {
+                            // DOWNGRADE
+                            bootbox.confirm({
+                                message: "Do you want to downgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        window.app.sendRestRequest('/managers/' + val.UserId, 'DELETE', null, function (data) {
+                                            bootbox.alert("This user is not a manager anymore");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        });
+                                    }
+                                }
+                            });
+                        }
                     });
                     $tr.find(".admin").click(function () {
-                        bootbox.confirm({
-                            message: "Do you want to upgrade this user ?",
-                            callback: function (result) {
-                                if (result) {
-                                    window.app.sendRestRequest('/admins', 'POST', null, function (data) {
-                                        bootbox.alert("This user is now an admin");
-                                        reloadAdminPanelPage();
-                                    }, function () {
-                                        bootbox.alert("An error has occured");
-                                    });
+                        if ($(this).is(":checked")) {
+                            bootbox.confirm({
+                                message: "Do you want to upgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        var data = { "UserId": val.UserId };
+                                        window.app.sendRestRequest('/admins', 'POST', data, function (data) {
+                                            bootbox.alert("Level up! This user is now an admin");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        }, 'application/json', true);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        } else {
+                            // DOWNGRADE
+                            bootbox.confirm({
+                                message: "Do you want to downgrade this user ?",
+                                callback: function (result) {
+                                    if (result) {
+                                        window.app.sendRestRequest('/admins/' + val.UserId, 'DELETE', null, function (data) {
+                                            bootbox.alert("This user is not an admin anymore");
+                                            reloadAdminPanelPage();
+                                        }, function () {
+                                            bootbox.alert("An error has occured");
+                                        });
+                                    }
+                                }
+                            });
+                        }
                     });
                 });
             });
