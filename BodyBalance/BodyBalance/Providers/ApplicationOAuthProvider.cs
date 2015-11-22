b832dc55a9d81ef3f0dbdd1d98fbf06e3064f704
priscarelique <prisca.relique@gmail.com>
2015-11-22 20:20:09 +0100

    Correction ApplicationOAuthProvider

--- BodyBalance/BodyBalance/Providers/ApplicationOAuthProvider.cs
+++ BodyBalance/BodyBalance/Providers/ApplicationOAuthProvider.cs
@@ -32,11 +32,7 @@ namespace BodyBalance.Providers
         {
             var userManager = new UserServices();
 
-<<<<<<< HEAD
             UserModel user = userManager.FindUserByIdAndPassword(context.UserName, context.Password);
-=======
-            UserModel user = userManager.FindUserByIdAndPassword(context.UserName,context.Password); 
->>>>>>> fb_registration
 
             if (user == null)
             {
